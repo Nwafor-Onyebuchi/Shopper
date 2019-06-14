41ebf41bc3b6c430e08f9af27ba9a4d718ca2843
Nwafor Onyebuchi <buchilazarus4@gmail.com>
2019-06-14 13:31:08 +0100

    github link added

--- client/src/components/AppNavbar.js
+++ client/src/components/AppNavbar.js
@@ -32,7 +32,7 @@ class AppNavbar extends Component {
                     <Collapse isOpen = {this.state.isOpen} navbar>
                         <Nav className = "ml-auto" navbar>
                             <NavItem>
-                                <NavLink href = "https://github.com.nwafor-onyebuchi" target = "_blank">GitHub</NavLink>
+                                <NavLink href = "https://github.com/Nwafor-Onyebuchi" target = "_blank">GitHub</NavLink>
                             </NavItem>
                         </Nav>
                     </Collapse>
